2ae148df053ccdf55f1770e673b099d66ac1e574
JHoo0118 <kik3078@gmail.com>
2022-08-21 15:27:24 +0900

    JWT Sign Test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,6 +1,5 @@
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { exec } from 'child_process';
 import { JwtService } from 'src/jwt/jwt.service';
 import { MailService } from 'src/mail/mail.service';
 import { Repository } from 'typeorm';
